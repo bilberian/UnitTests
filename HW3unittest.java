947abade41802c2c35d2d953e312421515e16cf1
bilberian <116255118+bilberian@users.noreply.github.com>
2023-10-17 19:38:01 +0300

    HW3

---
+++ HW3unittest.java
@@ -0,0 +1,11 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class HW3unittest {
+    public static boolean evenOddNumber(int n){
+        return n % 2 == 0;
+    }
+
+    public static boolean numberInInterval(int n){
+        return n >= 25 && n <= 100;
+    }
+}
